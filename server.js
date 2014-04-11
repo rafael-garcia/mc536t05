7f1527c76991ce337609f78bff4bd3a3bb3b6183
Rafael Garcia <rafamg93@gmail.com>
2014-04-10 22:04:18 -0300

    Configurado email de usuario. Adicionando dependencias MYSQL

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+var server = app.listen(8080, function() {
+    console.log('Listening on port %d', server.address().port);
+});
+
+
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+}
+
+// error
+app.use(errorHandler(err, req, res, next);
