8fdc8851a85e3455ceaed6ac89675e343b977407
Fellipe Caetano <fellipe.caetano4@gmail.com>
2014-04-14 20:12:33 -0300

    Listagem de artistas

--- routers/artistas.js
+++ routers/artistas.js
@@ -4,7 +4,9 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
     dbaccess.query('SELECT * FROM artista', function(err, rows, fields) {
-        res.send(rows);
+        res.render('artistas/index', {
+            artistas: rows
+        });
     });
 });
 
