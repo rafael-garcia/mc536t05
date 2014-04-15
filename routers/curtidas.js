1c9480203401d1e33ca686b05185215022919a24
Fellipe Caetano <fellipe.caetano4@gmail.com>
2014-04-14 21:09:29 -0300

    Codigo para atualizacao de curtidas

--- routers/curtidas.js
+++ routers/curtidas.js
@@ -16,4 +16,12 @@ router.post('/', function(req, res) {
     });
 });
 
+router.post('/:usuario/:artista', function(req, res) {
+    var query = 'UPDATE curtida SET usuario = ?, artista = ?, nota = ? WHERE usuario = ? AND artista = ?';
+    var params = [req.body.usuario, req.body.artista, req.body.nota, req.params.usuario, req.params.artista];
+    dbaccess.query(query, params, function(err, result) {
+        res.send(result);
+    });
+});
+
 module.exports = router;
