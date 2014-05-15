7f46cddd6cf0e316f919c2b374c4d9329cf65fd5
Rafael Garcia <rafamg93@gmail.com>
2014-05-15 20:57:04 -0300

    LastfmSearcher

---
+++ LastfmSearcher.js
@@ -0,0 +1,13 @@
+var util = require("util");
+var API_KEY = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=%s&api_key=8265bfa09c0cf54acaa62dd704340581&format=json";
+
+module.exports = {
+    getArtistInfo: function(artist, callback) {
+    	var query = util.format(API_KEY, artist);
+        request.get(query, callback);
+    },
+    getSimilarArtists: function(artist, callback) { //TODO: ainda nao definido
+    	var query = util.format(API_KEY, artist);
+        request.get(query, callback);
+    }
+};
